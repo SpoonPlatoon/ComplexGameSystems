96f8c969e1c4d8e5cfeeefb9612c666dc2d7505b
SpoonPlatoon <tycooperwilson@gmail.com>
2018-11-05 13:14:44 +1100

    Automated Testing test

---
+++ Assets/Examples/ScriptableObjects/Scenes/Tests/GameObjectCreationTest.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class GameObjectCreationTest
+{
+
+    [Test]
+    public void GameObjectCreationTestSimplePasses()
+    {
+        // Use the Assert class to test conditions.
+        var go = new GameObject("New GameObject");
+        Assert.AreEqual("New GameObject", go.name);
+    }
+
+    [UnityTest]
+    public IEnumerator RigidBodyTest()
+    {
+        var go = new GameObject(); //Create a new GameObject
+        go.AddComponent<Rigidbody>(); //Add Rigidbody to it
+
+        Transform t = go.transform; //Simplify transform variable name
+        Vector3 originalPos = t.position; //Copy original position
+
+        yield return new WaitForFixedUpdate(); //Wait till the FixedUpdate (physics) frame is complete
+
+        Assert.AreNotEqual(originalPos, t.position); //Compare original to current
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator GameObjectCreationTestWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+    }
+}
